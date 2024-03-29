382d50ccaf49db187e0302322117906cd188abab
Codemartino <akudugumartin5@gmail.com>
2024-03-29 14:09:08 +0000

    dummy data updated

--- react-social/src/dummyData.js
+++ react-social/src/dummyData.js
@@ -1,52 +1,52 @@
 export const Users = [
   {
     id:1,
-    profilePicture: "assets/person/1.jpg",
+    profilePicture: "post/person/1.jpg",
     username: "John Joana",
   },
   {
     id:2,
-    profilePicture: "assets/person/2.jpg",
+    profilePicture: "post/person/2.jpg",
     username: "Delwin Jnr",
   },
   {
     id:3,
-    profilePicture: "assets/person/3.jpg",
+    profilePicture: "post/person/3.jpg",
     username: "Monalisa Edem",
   },
   {
     id:4,
-    profilePicture: "assets/person/4.jpg",
+    profilePicture: "post/person/4.jpg",
     username: "Sir Martin",
   },
   {
     id:5,
-    profilePicture: "assets/person/5.jpg",
+    profilePicture: "post/person/5.jpg",
     username: "Solace Skylar",
   },
   {
     id:6,
-    profilePicture: "assets/person/6.jpg",
+    profilePicture: "post/person/6.jpg",
     username: "Edna Owusu",
   },
   {
     id:7,
-    profilePicture: "assets/person/7.jpg",
+    profilePicture: "post/person/7.jpg",
     username: "Efo Buju",
   },
   {
     id:8,
-    profilePicture: "assets/person/8.jpg",
+    profilePicture: "post/person/8.jpg",
     username: "Sir Code",
   },
   {
     id:9,
-    profilePicture: "assets/person/9.jpg",
+    profilePicture: "post/person/9.jpg",
     username: "Kwame Anti",
   },
   {
     id:10,
-    profilePicture: "assets/person/10.jpeg",
+    profilePicture: "post/person/10.jpeg",
     username: "Mark Azumah",
   },
 ];
@@ -55,7 +55,7 @@ export const Posts = [
   {
     id: 1,
     desc: "pokuase interchange located in Accra,Ghana.",
-    photo: "assets/post/1.jpg",
+    photo: "post/post/1.jpg",
     date: "5 mins ago",
     userId: 1,
     like: 32,
@@ -63,7 +63,7 @@ export const Posts = [
   },
   {
     id: 2,
-    photo: "assets/post/2.jpg",
+    photo: "post/post/2.jpg",
     date: "15 mins ago",
     userId: 2,
     like: 2,
@@ -72,7 +72,7 @@ export const Posts = [
   {
     id: 3,
     desc: "Every moment is a fresh beginning.",
-    photo: "assets/post/11.jpg",
+    photo: "post/post/11.jpg",
     date: "1 hour ago",
     userId: 3,
     like: 61,
@@ -80,7 +80,7 @@ export const Posts = [
   },
   {
     id: 4,
-    photo: "assets/post/4.jpg",
+    photo: "post/post/4.jpg",
     date: "4 hours ago",
     userId: 4,
     like: 7,
@@ -88,7 +88,7 @@ export const Posts = [
   },
   {
     id: 5,
-    photo: "assets/post/5.jpg",
+    photo: "post/post/5.jpg",
     date: "5 hours ago",
     userId: 5,
     like: 23,
@@ -96,7 +96,7 @@ export const Posts = [
   },
   {
     id: 6,
-    photo: "assets/post/6.jpg",
+    photo: "post/post/6.jpg",
     date: "1 day ago",
     userId: 6,
     like: 44,
@@ -105,7 +105,7 @@ export const Posts = [
   {
     id: 7,
     desc: "Never regret anything that made you smile.",
-    photo: "assets/post/7.jpg",
+    photo: "post/post/7.jpg",
     date: "2 days ago",
     userId: 7,
     like: 52,
@@ -113,7 +113,7 @@ export const Posts = [
   },
   {
     id: 8,
-    photo: "assets/post/8.jpg",
+    photo: "post/post/8.jpg",
     date: "3 days ago",
     userId: 8,
     like: 15,
@@ -122,7 +122,7 @@ export const Posts = [
   {
     id: 9,
     desc: "Waakye, a Ghanaian meal.",
-    photo: "assets/post/9.jpg",
+    photo: "post/post/9.jpg",
     date: "5 days ago",
     userId: 9,
     like: 11,
@@ -130,7 +130,7 @@ export const Posts = [
   },
   {
     id: 10,
-    photo: "assets/post/10.jpg",
+    photo: "post/post/10.jpg",
     date: "1 week ago",
     userId: 10,
     like: 104,
